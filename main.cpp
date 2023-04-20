b10062d2e7652626c44c93030741cb5ff8c1d24e
KeroppiKun <fuga0809ace@gmail.com>
2023-04-21 03:12:33 +0900

    MT3_01_00課題

--- main.cpp
+++ main.cpp
@@ -12,10 +12,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 scale(1.2f, 0.79f, -2.1f);
-	Vector3 rotate(0.4f, 1.43f, -0.8f);
-	Vector3 translate(2.7f, -4.15f, 1.57f);
-	Matrix4x4 worldMatrix = Matrix4x4::MakeAffineMatrix(scale, rotate, translate);
+	Matrix4x4 orthographicMatrix = Matrix4x4::MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovMatrix = Matrix4x4::MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = Matrix4x4::MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -37,7 +36,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		Function::MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		Function::MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		Function::MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		Function::MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
